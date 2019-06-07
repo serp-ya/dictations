f533073a881d5153c23c10f43110aba539eaa571
serp-ya <sergei.kundryukov1989@gmail.com>
2019-06-08 00:49:20 +0300

    Updates

---
+++ precache-manifest.e5ee505673fb23cda8899cf4910c28e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0009bae7286dffef061681bf2beea41b",
+    "url": "/dictations/index.html"
+  },
+  {
+    "revision": "6d4710cfbf688bd92893",
+    "url": "/dictations/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6d4710cfbf688bd92893",
+    "url": "/dictations/static/js/2.9024c117.chunk.js"
+  },
+  {
+    "revision": "5c2ef67ca901d1144251",
+    "url": "/dictations/static/js/main.30ba5637.chunk.js"
+  },
+  {
+    "revision": "2361c90ea87c53a9a413",
+    "url": "/dictations/static/js/runtime~main.eeffa2b3.js"
+  }
+]);
